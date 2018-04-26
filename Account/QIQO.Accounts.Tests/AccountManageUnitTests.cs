d3d3b6208957b2caaf107d065db4c5b9dca436e1
R Richards <rdrrichards@hotmail.com>
2018-04-25 19:24:06 -0500

    unit test prep

--- Account/QIQO.Accounts.Tests/AccountManageUnitTests.cs
+++ Account/QIQO.Accounts.Tests/AccountManageUnitTests.cs
@@ -1,15 +1,34 @@
+using Microsoft.Extensions.Logging;
+using Moq;
+using QIQO.Accounts.Data;
 using QIQO.Accounts.Manager;
+using QIQO.MQ;
 using Xunit;
 
 namespace QIQO.Accounts.Tests
 {
     public class AccountManageUnitTests
     {
-        [Fact]
-        public void Test1()
+        private readonly Mock<ILogger<AccountsManager>> _mockLog;
+        private readonly Mock<IMQPublisher> _mqPublisher;
+        private readonly Mock<IAccountRepository> _accountRepository;
+        private readonly Mock<IAccountEntityService> _accountEntityService;
+
+        public AccountManageUnitTests()
         {
-            //var sut = new Test();
-            //Assert.True(sut.TestName == null);
+            _mockLog = new Mock<ILogger<AccountsManager>>();
+            _mqPublisher = new Mock<IMQPublisher>();
+            _accountRepository  = new Mock<IAccountRepository>();
+            _accountEntityService = new Mock<IAccountEntityService>();
+        }
+        [Fact]
+        public async void AccountsManager_GetAccountsAsync_IsEmpty()
+        {
+            var sut = new AccountsManager(_mockLog.Object, _mqPublisher.Object, _accountRepository.Object, _accountEntityService.Object);
+
+            var retVal = await sut.GetAccountsAsync();
+
+            Assert.True(retVal.Count == 0);
         }
         [Fact]
         public void Test2()
