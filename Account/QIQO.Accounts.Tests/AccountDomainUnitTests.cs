e478709e828b3947f2813a57b49a3c1e51200b59
R Richards <rdrrichards@hotmail.com>
2018-04-30 21:02:10 -0500

    unit test

--- Account/QIQO.Accounts.Tests/AccountDomainUnitTests.cs
+++ Account/QIQO.Accounts.Tests/AccountDomainUnitTests.cs
@@ -7,7 +7,7 @@ namespace QIQO.Accounts.Tests
     public class AccountDomainUnitTests
     {
         [Fact]
-        public void AccountModelTest()
+        public void AccountModelTest1()
         {
             var data = new AccountData {
                 AccountKey = 1,
@@ -38,6 +38,40 @@ namespace QIQO.Accounts.Tests
             Assert.True(sut.AccountEndDate == new System.DateTime(2019, 1, 1));
             Assert.True(sut.AddedDateTime == new System.DateTime(2018, 1, 1));
             Assert.True(sut.UpdateDateTime == new System.DateTime(2018, 1, 1));
+
+            Assert.True(sut.Comments.Count == 0);
+            Assert.True(sut.Contacts.Count == 0);
+            Assert.True(sut.Addresses.Count == 0);
+            Assert.True(sut.FeeSchedules.Count == 0);
+            Assert.True(sut.Employees.Count == 0);
+            Assert.True(sut.AccountAttributes.Count == 0);
+        }
+
+        [Fact]
+        public void AccountModelTest2()
+        {
+            var sut = new Account(1, QIQOAccountType.Individual, "TEST", "TEST", "TEST", "TEST",
+                new System.DateTime(2018, 1, 1), new System.DateTime(2019, 1, 1));
+            Assert.True(sut.AccountKey == 0);
+            Assert.True(sut.CompanyKey == 1);
+            Assert.True(sut.AccountType == QIQOAccountType.Individual);
+            Assert.True(sut.AccountCode == "TEST");
+            Assert.True(sut.AccountName == "TEST");
+            Assert.True(sut.AccountDesc == "TEST");
+            Assert.True(sut.AccountDba == "TEST");
+            Assert.True(sut.AddedUserID == null);
+            Assert.True(sut.UpdateUserID == null);
+            Assert.True(sut.AccountStartDate == new System.DateTime(2018, 1, 1));
+            Assert.True(sut.AccountEndDate == new System.DateTime(2019, 1, 1));
+            Assert.True(sut.AddedDateTime != null);
+            Assert.True(sut.UpdateDateTime != null);
+
+            Assert.True(sut.Comments.Count == 0);
+            Assert.True(sut.Contacts.Count == 0);
+            Assert.True(sut.Addresses.Count == 0);
+            Assert.True(sut.FeeSchedules.Count == 0);
+            Assert.True(sut.Employees.Count == 0);
+            Assert.True(sut.AccountAttributes.Count == 0);
         }
     }
 }
