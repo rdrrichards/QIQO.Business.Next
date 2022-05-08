efe58ff7bee0ad1d0f8b622f42b8f5e796c8e3d0
R. Richards <rdrrichards@hotmail.com>
2022-05-08 05:34:59 -0500

    basic execution working

--- Product/QIQO.Products.Api/Program.cs
+++ Product/QIQO.Products.Api/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddApiVersioning();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
