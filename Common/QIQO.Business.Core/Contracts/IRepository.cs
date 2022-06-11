3caab3a018f9348548a2024d51a29ef3b4d82cc1
R. Richards <rdrrichards@hotmail.com>
2022-06-11 12:25:27 -0500

    interface refactor

--- Common/QIQO.Business.Core/Contracts/IRepository.cs
+++ Common/QIQO.Business.Core/Contracts/IRepository.cs
@@ -2,22 +2,26 @@
 
 namespace QIQO.Business.Core.Contracts
 {
-    public interface IRepository
-    {
+    public interface IRepository { }
 
-    }
-
-    public interface IRepository<T> : IRepository // Think about splitting this up (LSP)
+    public interface IReadRepository<T> : IRepository
         where T : class, IEntity, new()
     {
         IEnumerable<T> GetAll();
         T GetByID(int entity_key);
         T GetByCode(string account_code, string entity_code);
+    }
+
+    public interface IWriteRepository<T> : IRepository
+        where T : class, IEntity, new()
+    {
         void Insert(T entity);
         void Delete(T entity);
         void DeleteByID(int entity_key);
         void DeleteByCode(string entity_code);
         void Save(T entity);
     }
+
+    public interface IRepository<T> : IReadRepository<T>, IWriteRepository<T> where T : class, IEntity, new()  { }
 
 }
