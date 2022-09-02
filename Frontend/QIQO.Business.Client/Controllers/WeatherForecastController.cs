4c7163fa22801a849435417642db0d4af6373fb0
R. Richards <rdrrichards@hotmail.com>
2022-09-02 06:22:12 -0500

    bff

---
+++ Frontend/QIQO.Business.Client/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QIQO.Business.Client.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly DaprClient _daprClient;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, DaprClient daprClient)
+        {
+            _logger = logger;
+            _daprClient = daprClient;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AccountData[]>>> Get(CancellationToken cancellationToken)
+        {
+            // var result = await _daprClient.InvokeMethodAsync("account-api", "accounts", cancellationToken);
+            var result = _daprClient.CreateInvokeMethodRequest(HttpMethod.Get, "accounts-api", "api/v1/accounts");
+            var accounts = await _daprClient.InvokeMethodAsync<AccountData[]>(result, cancellationToken);
+            return Ok(accounts);
+        }
+    }
+
+    public class AccountData
+    {
+        public int AccountKey { get; set; }
+        public int CompanyKey { get; set; }
+        public int AccountType { get; set; }
+        public string AccountCode { get; set; } = string.Empty;
+        public string AccountName { get; set; } = string.Empty;
+        public string AccountDesc { get; set; } = string.Empty;
+        public string AccountDba { get; set; } = string.Empty;
+        public DateTime AccountStartDate { get; set; }
+        public DateTime? AccountEndDate { get; set; }
+        public string AddedUserID { get; set; } = string.Empty;
+        public DateTime AddedDateTime { get; set; }
+        public string UpdateUserID { get; set; } = string.Empty;
+        public DateTime? UpdateDateTime { get; set; }
+    }
+
+}
---
+++ Frontend/QIQO.Business.Client/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var daprHttpPort = Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "3601";
+var daprGrpcPort = Environment.GetEnvironmentVariable("DAPR_GRPC_PORT") ?? "60001";
+
+builder.Services.AddDaprClient(builder => builder
+    .UseHttpEndpoint($"http://localhost:{daprHttpPort}")
+    .UseGrpcEndpoint($"http://localhost:{daprGrpcPort}"));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllersWithViews().AddDapr();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html"); ;
+
+app.Run();
