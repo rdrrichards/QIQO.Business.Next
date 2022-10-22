67f01e71dd4789e7bd731e2c0d6f8fc0e1668544
R. Richards <rdrrichards@hotmail.com>
2022-10-22 09:43:07 -0500

    new source gen project addition

---
+++ Common/QIQO.Code.Generators/DataAccessLayerGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+namespace QIQO.Code.Generators
+{
+    [Generator]
+    public class DataAccessLayerGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
